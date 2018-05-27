f8abe5bebe8c5d02f6374bfaec841f200cb5d75f
Marcin <emwiechnik@gmail.com>
2018-05-27 22:06:34 +0200

    added basic handling of couchdb - pouchdb sync - to be refactored later

--- src/services/pouchdb.instance.js
+++ src/services/pouchdb.instance.js
@@ -1,7 +1,53 @@
 import PouchDB from 'pouchdb'
 
+const COUCHDB_URL = 'http://127.0.0.1:5984'
 const dbName = 'itemsDb'
+const userName = 'user1'
+const pwd = 'user1pass' // temporarily hardcoded
 
-let db = new PouchDB(dbName)
+const db = new PouchDB(dbName)
+console.log('Created local db connection')
+
+const creds = { name: userName, password: pwd }
+
+function toHex (s) {
+  let u = unescape(encodeURIComponent(s))
+  let h = ''
+  for (var i = 0; i < u.length; i++) {
+    h += u.charCodeAt(i).toString(16)
+  }
+  return h
+}
+
+const hexEncodedUserName = toHex(userName)
+
+// connect to the remote db
+fetch(`${COUCHDB_URL}/_session`, {
+  method: 'POST',
+  body: JSON.stringify(creds),
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}).then(res => res.json())
+  .catch(error => console.error('Error:', error))
+  .then(response => console.log('Success:', response))
+
+const remoteDatabase = new PouchDB(`${COUCHDB_URL}/userdb-${hexEncodedUserName}`, {
+  skipSetup: true,
+  ajax: {
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8'
+    }
+  }
+})
+console.log('Created remote db connection')
+
+console.log('Requested synching')
+PouchDB.sync(db, remoteDatabase, {
+  live: false,
+  heartbeat: false,
+  timeout: false,
+  retry: true
+})
 
 export default db
