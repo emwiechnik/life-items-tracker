931684b6ff8e5b7a4e73a2635ecca5cc59e2f5a6
Marcin <emwiechnik@gmail.com>
2018-06-09 20:11:55 +0200

    #28 added firebase and wrapped it into a cloudService

--- src/main.js
+++ src/main.js
@@ -6,16 +6,18 @@ import router from './router'
 import store from './stateMgmt/rootStore'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
-
+import cloudService from './services/cloud.service'
 Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  store,
-  components: { App },
-  template: '<App/>'
+cloudService.init().then(() => {
+  /* eslint-disable no-new */
+  new Vue({
+    el: '#app',
+    router,
+    store,
+    components: { App },
+    template: '<App/>'
+  })
 })
---
+++ src/services/cloud.service.js
@@ -0,0 +1,55 @@
+import firebase from 'firebase'
+
+class UserDetails {
+  constructor (uid, name, fullName, email, avatarUrl) {
+    this.uid = uid
+    this.name = name
+    this.fullName = fullName
+    this.email = email
+    this.avatarUrl = avatarUrl
+  }
+}
+
+const cloudService = {
+  init () {
+    let config = {
+      apiKey: 'AIzaSyB9zjooxD0njIrxFeM2Arn30tJ5BDNt8o4',
+      authDomain: 'myfirstfirebaseproject-d7d27.firebaseapp.com',
+      databaseURL: 'https://myfirstfirebaseproject-d7d27.firebaseio.com',
+      projectId: 'myfirstfirebaseproject-d7d27',
+      storageBucket: 'myfirstfirebaseproject-d7d27.appspot.com',
+      messagingSenderId: '400499969549'
+    }
+
+    firebase.initializeApp(config)
+
+    return new Promise((resolve, reject) => {
+      firebase.auth().onAuthStateChanged(resolve, reject)
+    })
+  },
+  signInWithEmailAndPassword () {
+    return new Promise((resolve, reject) => {
+      firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(() => { // todo: see if you can ignore argument
+        resolve()
+      }, err => {
+        reject(err)
+      })
+    })
+  },
+  signInUsingGoogle () {
+    return new Promise((resolve, reject) => {
+      let provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithRedirect(provider).then(user => {
+        resolve()
+      }, err => {
+        reject(err)
+      })
+    })
+  },
+  currentUser () {
+    let user = firebase.auth().currentUser
+    return user ? new UserDetails(user.uid, user.displayName, user.displayName, user.email, user.photoURL) : user
+  }
+}
+
+export default cloudService
